d1c43d960c68c1e966eb351ffa50beafda929527
Kelly Selden <kellyselden@gmail.com>
2013-07-12 18:20:42 -0400

    changes

--- KellySelden.Libraries/KellySelden.Libraries.EntityFramework/EntityFrameworkBatchProcess.cs
+++ KellySelden.Libraries/KellySelden.Libraries.EntityFramework/EntityFrameworkBatchProcess.cs
@@ -13,16 +13,20 @@ namespace KellySelden.Libraries.EntityFramework
 		const BindingFlags Flags = BindingFlags.Instance | BindingFlags.NonPublic;
 
 		readonly Type _entityType;
+		readonly IDictionary<string, PropertyInfo> _propertyLookup;
 		readonly IEnumerable<string> _keyProperties, _ignoredProperties;
-		readonly object _properties;
+		readonly object _mappedProperties;
 		readonly MethodInfo _processPropertiesMethod;
+		readonly SqlBatchProcess _sqlBatchProcess;
+
 		Type _primitivePropertyConfigurationType;
 		PropertyInfo _columnNameProperty, _databaseGeneratedOptionProperty;
-		readonly SqlBatchProcess _sqlBatchProcess;
+		T[] _inserts;
 
 		public EntityFrameworkBatchProcess(string connectionString, EntityTypeConfiguration<T> map) //make map nullable
 		{
 			_entityType = typeof(T);
+			_propertyLookup = _entityType.GetProperties().ToDictionary(p => p.Name);
 
 			object configuration = map.GetType().GetProperty("Configuration", Flags).GetValue(map, null);
 			Type configurationType = configuration.GetType();
@@ -30,46 +34,40 @@ namespace KellySelden.Libraries.EntityFramework
 			_keyProperties = ((IEnumerable<PropertyInfo>)configurationType.GetField("_keyProperties", Flags).GetValue(configuration)).Select(p => p.Name);
 			_ignoredProperties = ((IEnumerable<PropertyInfo>)configurationType.GetProperty("IgnoredProperties", Flags).GetValue(configuration, null)).Select(p => p.Name);
 
-			_properties = configurationType.GetProperty("PrimitivePropertyConfigurations", Flags).GetValue(configuration, null);
+			_mappedProperties = configurationType.GetProperty("PrimitivePropertyConfigurations", Flags).GetValue(configuration, null);
 			_processPropertiesMethod = typeof(EntityFrameworkBatchProcess<T>).GetMethod("ProcessProperties", Flags)
-				.MakeGenericMethod(_properties.GetType().GetInterfaces().Single(i =>
+				.MakeGenericMethod(_mappedProperties.GetType().GetInterfaces().Single(i =>
 					i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IDictionary<,>)).GetGenericArguments());
 			
 			object databaseName = configurationType.GetMethod("GetTableName", Flags).Invoke(configuration, null);
 			var tableName = databaseName == null ? _entityType.Name : (string)databaseName.GetType().GetProperty("Name").GetValue(databaseName, null);
 			_sqlBatchProcess = new SqlBatchProcess(connectionString, tableName);
 		}
-		
+
 		public void AddEntitiesToInsert(IEnumerable<T> entities)
 		{
-			foreach (T entity in entities)
-				AddEntityToInsert(entity);
+			_inserts = entities.ToArray();
+			AddEntities(_inserts, DbOperation.Insert);
 		}
 		public void AddEntitiesToUpdate(IEnumerable<T> entities)
 		{
-			foreach (T entity in entities)
-				AddEntityToUpdate(entity);
+			AddEntities(entities, DbOperation.Update);
 		}
 		public void AddEntitiesToDelete(IEnumerable<T> entities)
 		{
+			AddEntities(entities, DbOperation.Delete);
+		}
+		void AddEntities(IEnumerable<T> entities, DbOperation operation)
+		{
 			foreach (T entity in entities)
-				AddEntityToDelete(entity);
-		}
-		public void AddEntityToInsert(T entity)
-		{
-			_processPropertiesMethod.Invoke(this, new[] { entity, _properties, DbOperation.Insert });
-		}
-		public void AddEntityToUpdate(T entity)
-		{
-			_processPropertiesMethod.Invoke(this, new[] { entity, _properties, DbOperation.Update });
-		}
-		public void AddEntityToDelete(T entity)
-		{
-			_processPropertiesMethod.Invoke(this, new[] { entity, _properties, DbOperation.Delete });
+				_processPropertiesMethod.Invoke(this, new[] { entity, _mappedProperties, operation });
 		}
 
 		enum DbOperation { Insert, Update, Delete }
 
+		KeyValuePair<string, Type>? _identity;
+		readonly IDictionary<string, string> _columnNameLookup = new Dictionary<string, string>();
+		readonly IDictionary<string, DatabaseGeneratedOption?> _databaseGeneratedOptionLookup = new Dictionary<string, DatabaseGeneratedOption?>();
 		void ProcessProperties<TKey, TValue>(T entity, IDictionary<TKey, TValue> mappedProperties, DbOperation operation)
 		{
 			var mappedPropertyNames = new List<string>();
@@ -86,9 +84,12 @@ namespace KellySelden.Libraries.EntityFramework
 				string propertyName = kvp.Key.ToString();
 				mappedPropertyNames.Add(propertyName);
 
-				var columnName = (string)_columnNameProperty.GetValue(kvp.Value, null) ?? propertyName; //property was mapped to key, but not as property
-				object columnValue = _entityType.GetProperty(propertyName).GetValue(entity, null);
-				
+				string columnName = (_columnNameLookup.ContainsKey(propertyName)
+					? _columnNameLookup[propertyName]
+					: _columnNameLookup[propertyName] = (string)_columnNameProperty.GetValue(kvp.Value, null))
+					?? propertyName; //property was mapped to key, but not as property
+				object columnValue = _propertyLookup[propertyName].GetValue(entity, null);
+
 				if (_keyProperties.Contains(propertyName))
 				{
 					if (operation != DbOperation.Insert)
@@ -100,15 +101,21 @@ namespace KellySelden.Libraries.EntityFramework
 					if (_databaseGeneratedOptionProperty == null)
 						_databaseGeneratedOptionProperty = _primitivePropertyConfigurationType.GetProperty("DatabaseGeneratedOption");
 
-					var databaseGeneratedOption = (DatabaseGeneratedOption?)_databaseGeneratedOptionProperty.GetValue(kvp.Value, null);
+					DatabaseGeneratedOption? databaseGeneratedOption = _databaseGeneratedOptionLookup.ContainsKey(propertyName)
+						? _databaseGeneratedOptionLookup[propertyName]
+						: _databaseGeneratedOptionLookup[propertyName] = (DatabaseGeneratedOption?)_databaseGeneratedOptionProperty.GetValue(kvp.Value, null);
 					if (!databaseGeneratedOption.HasValue || databaseGeneratedOption.Value == DatabaseGeneratedOption.Identity)
+					{
+						if (_identity == null)
+							_identity = new KeyValuePair<string, Type>(propertyName, columnValue.GetType());
 						continue;
+					}
 				}
 
 				if (operation != DbOperation.Delete)
 					columns.Add(columnName, columnValue);
 			}
-			foreach (PropertyInfo property in _entityType.GetProperties())
+			foreach (PropertyInfo property in _propertyLookup.Values)
 			{
 				if (!mappedPropertyNames.Contains(property.Name)
 					&& !_ignoredProperties.Contains(property.Name)
@@ -118,7 +125,7 @@ namespace KellySelden.Libraries.EntityFramework
 			switch (operation)
 			{
 				case DbOperation.Insert:
-					_sqlBatchProcess.AddRowToInsert(columns);
+					_sqlBatchProcess.AddRowToInsert(columns, _identity);
 					break;
 				case DbOperation.Update:
 					_sqlBatchProcess.AddRowToUpdate(keyColumns, columns);
@@ -129,9 +136,14 @@ namespace KellySelden.Libraries.EntityFramework
 			}
 		}
 
-		public void Process(int? timeout = null, int batchSize = 20000)
+		public void Process(int? timeout = null, int batchSize = SqlBatchProcess.DefaultBatchSize)
 		{
-			_sqlBatchProcess.Process(timeout, batchSize);
+			object[] identityKeys = _sqlBatchProcess.Process(timeout, batchSize).ToArray();
+			for (int i = 0; i < identityKeys.Length; i++)
+			{
+				_propertyLookup[_identity.Value.Key].SetValue(_inserts[i], identityKeys[i], null);
+			}
+			_inserts = null;
 		}
 
 		public void Dispose()
--- KellySelden.Libraries/KellySelden.Libraries.Sql/SqlBatchProcess.cs
+++ KellySelden.Libraries/KellySelden.Libraries.Sql/SqlBatchProcess.cs
@@ -9,13 +9,16 @@ namespace KellySelden.Libraries.Sql
 {
 	public class SqlBatchProcess : IDisposable
 	{
+		public const int DefaultBatchSize = 20000;
+
 		readonly SqlConnectionWrapper _connectionWrapper;
 		readonly string _tableName;
 		readonly SqlDataAdapter _sqlDataAdapter;
-		readonly List<IDictionary<string, object>> _insertRows = new List<IDictionary<string, object>>();
+
+		readonly List<KeyValuePair<KeyValuePair<string, Type>?, IDictionary<string, object>>> _insertRows = new List<KeyValuePair<KeyValuePair<string, Type>?, IDictionary<string, object>>>();
 		readonly List<KeyValuePair<IDictionary<string, object>, IDictionary<string, object>>> _updateRows = new List<KeyValuePair<IDictionary<string, object>, IDictionary<string, object>>>();
 		readonly List<IDictionary<string, object>> _deleteRows = new List<IDictionary<string, object>>();
-		
+
 		/// <param name="tableName">don't ever accept user input as the table name (SQL injection)</param>
 		public SqlBatchProcess(string connectionString, string tableName) : this(new SqlConnectionWrapper(connectionString), tableName) { }
 		/// <param name="tableName">don't ever accept user input as the table name (SQL injection)</param>
@@ -27,11 +30,11 @@ namespace KellySelden.Libraries.Sql
 			_sqlDataAdapter = new SqlDataAdapter();
 		}
 
-		public void AddRowToInsert(IDictionary<string, object> columns)
+		public void AddRowToInsert(IDictionary<string, object> columns, KeyValuePair<string, Type>? output = null)
 		{
 			int row = _insertRows.Count;
-			AddEmptyRowsToInsert(row);
-			AddRow(_insertRows[row], columns);
+			AddEmptyRowsToInsert(row, output);
+			AddRow(_insertRows[row].Value, columns);
 		}
 		public void AddRowToUpdate(IDictionary<string, object> keys, IDictionary<string, object> columns)
 		{
@@ -49,38 +52,13 @@ namespace KellySelden.Libraries.Sql
 		void AddRow(IDictionary<string, object> row, IEnumerable<KeyValuePair<string, object>> columns)
 		{
 			foreach (KeyValuePair<string, object> column in columns)
-				AddColumn(row, column.Key, column.Value);
+				row.Add(column.Key, column.Value);
 		}
 
-		public void AddColumnToInsert(int row, string column, object value)
-		{
-			AddEmptyRowsToInsert(row);
-			AddColumn(_insertRows[row], column, value);
-		}
-		public void AddKeyForUpdate(int row, string column, object value)
-		{
-			AddEmptyRowsToUpdate(row);
-			AddColumn(_updateRows[row].Key, column, value);
-		}
-		public void AddColumnToUpdate(int row, string column, object value)
-		{
-			AddEmptyRowsToUpdate(row);
-			AddColumn(_deleteRows[row], column, value);
-		}
-		public void AddKeyForDelete(int row, string column, object value)
-		{
-			AddEmptyRowsToDelete(row);
-			AddColumn(_deleteRows[row], column, value);
-		}
-		void AddColumn(IDictionary<string, object> row, string column, object value)
-		{
-			row.Add(column, value);
-		}
-
-		void AddEmptyRowsToInsert(int row)
+		void AddEmptyRowsToInsert(int row, KeyValuePair<string, Type>? output)
 		{
 			while (_insertRows.Count <= row)
-				_insertRows.Add(new Dictionary<string, object>());
+				_insertRows.Add(new KeyValuePair<KeyValuePair<string, Type>?, IDictionary<string, object>>(output, new Dictionary<string, object>()));
 		}
 		void AddEmptyRowsToUpdate(int row)
 		{
@@ -93,35 +71,38 @@ namespace KellySelden.Libraries.Sql
 				_deleteRows.Add(new Dictionary<string, object>());
 		}
 
-		public void Process(int? timeout = null, int batchSize = 20000)
+		public IEnumerable<object> Process(int? timeout = null, int batchSize = DefaultBatchSize)
 		{
-			IDictionary<string, object> insertRow = _insertRows.FirstOrDefault();
+			KeyValuePair<KeyValuePair<string, Type>?, IDictionary<string, object>> insertRow = _insertRows.FirstOrDefault();
 			KeyValuePair<IDictionary<string, object>, IDictionary<string, object>> updateRow = _updateRows.FirstOrDefault();
 			IDictionary<string, object> deleteRow = _deleteRows.FirstOrDefault();
-			if (insertRow == null && updateRow.Key == null && deleteRow == null)
-				return;
+			if (insertRow.Value == null && updateRow.Value == null && deleteRow == null)
+				return new object[0];
 
 			var table = new DataTable(_tableName);
-			table.Columns.AddRange(GetColumnsForUnion(insertRow)
+			table.Columns.AddRange((insertRow.Key == null ? new string[0] : new[] { insertRow.Key.Value.Key })
+				.Union(GetColumnsForUnion(insertRow.Value)
 				.Union(GetColumnsForUnion(updateRow.Key)
-				.Union(GetColumnsForUnion(updateRow.Value))
-				.Union(GetColumnsForUnion(deleteRow)))
+				.Union(GetColumnsForUnion(updateRow.Value)
+				.Union(GetColumnsForUnion(deleteRow)))))
 				.Select(c => new DataColumn(c)).ToArray());
 
-			if (insertRow != null)
+			var insertedRows = new List<DataRow>();
+			if (insertRow.Value != null)
 			{
-				AddRows(table, _insertRows);
+				AddRows(table, _insertRows.Select(r => r.Value), insertedRows.Add);
 				if (_sqlDataAdapter.InsertCommand == null)
 				{
 					_sqlDataAdapter.InsertCommand = PrepareCommand(
-						string.Format("INSERT INTO {0} ({1}) VALUES ({2})", _tableName,
-							string.Join(", ", insertRow.Keys),
-							string.Join(", ", insertRow.Keys.Select(k => "@" + k))),
-						timeout, insertRow);
+						string.Format("INSERT INTO {0} ({1}) VALUES ({2}){3}", _tableName,
+							string.Join(", ", insertRow.Value.Keys),
+							string.Join(", ", insertRow.Value.Keys.Select(k => '@' + k)),
+							insertRow.Key != null ? string.Format(" SET @{0} = SCOPE_IDENTITY()", insertRow.Key.Value.Key) : ""),
+						timeout, insertRow.Value, insertRow.Key);
 				}
 			}
 
-			if (updateRow.Key != null)
+			if (updateRow.Value != null)
 			{
 				AddRows(table, _updateRows.Select(r => r.Key.Union(r.Value)), row =>
 				{
@@ -156,10 +137,12 @@ namespace KellySelden.Libraries.Sql
 
 			_sqlDataAdapter.UpdateBatchSize = batchSize;
 			_sqlDataAdapter.Update(table);
-
+			
 			_insertRows.Clear();
 			_updateRows.Clear();
 			_deleteRows.Clear();
+
+			return insertRow.Key == null ? new object[0] : insertedRows.Select(r => Convert.ChangeType(r[insertRow.Key.Value.Key], insertRow.Key.Value.Value));
 		}
 
 		IEnumerable<string> GetColumnsForUnion(IDictionary<string, object> columns)
@@ -179,13 +162,22 @@ namespace KellySelden.Libraries.Sql
 			}
 		}
 
-		SqlCommand PrepareCommand(string sql, int? timeout, IEnumerable<KeyValuePair<string, object>> row)
+		SqlCommand PrepareCommand(string sql, int? timeout, IEnumerable<KeyValuePair<string, object>> row, KeyValuePair<string, Type>? output = null)
 		{
-			var command = new SqlCommand(sql, _connectionWrapper.Connection) { UpdatedRowSource = UpdateRowSource.None };
+			var command = new SqlCommand(sql, _connectionWrapper.Connection);
 			if (timeout.HasValue)
 				command.CommandTimeout = timeout.Value;
 			foreach (var column in row)
 				command.Parameters.Add('@' + column.Key, Helpers.ConvertToSqlDbType(column.Value.GetType())).SourceColumn = column.Key;
+			if (output == null)
+				command.UpdatedRowSource = UpdateRowSource.None;
+			else
+			{
+				var outputParam = command.Parameters.Add('@' + output.Value.Key, Helpers.ConvertToSqlDbType(output.Value.Value));
+				outputParam.SourceColumn = output.Value.Key;
+				outputParam.Direction = ParameterDirection.Output;
+				command.UpdatedRowSource = UpdateRowSource.OutputParameters;
+			}
 			return command;
 		}
 

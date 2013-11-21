b32d72efb2a09792edaae02a341288b5f6c13477
Kelly Selden <kellyselden@gmail.com>
2013-11-20 22:20:53 -0500

    utilize sql rounding with concrete types

---
+++ KellySelden.Libraries/KellySelden.Libraries.Sql/Math/SqlDecimalAny.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlTypes;
+
+namespace KellySelden.Libraries.Sql.Math
+{
+	public sealed class SqlDecimalAny : SqlDecimalBase
+	{
+		internal SqlDecimalAny(SqlDecimal d) : base(d) { }
+
+		public static implicit operator SqlDecimalAny(decimal d) { return new SqlDecimalAny(d); }
+	}
+}
---
+++ KellySelden.Libraries/KellySelden.Libraries.Sql/Math/SqlDecimalExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KellySelden.Libraries.Sql.Math
+{
+	public static class SqlDecimalExtensions
+	{
+		public static SqlDecimalAny Average(this IEnumerable<SqlDecimalBase> source)
+		{
+			var s = source.ToArray();
+			return s.Sum() / s.Length;
+		}
+		public static SqlDecimalAny Average<T>(this IEnumerable<T> source, Func<T, SqlDecimalBase> selector)
+		{
+			return Average(source.Select(selector));
+		}
+
+		public static SqlDecimalAny Sum(this IEnumerable<SqlDecimalBase> source)
+		{
+			SqlDecimalAny sum = SqlDecimalBase.Null;
+			foreach (SqlDecimalBase d in source)
+			{
+				if (!SqlDecimalBase.IsNull(d))
+				{
+					if (sum == SqlDecimalBase.Null) sum = 0;
+					sum += d;
+				}
+			}
+			return sum;
+		}
+		public static SqlDecimalAny Sum<T>(this IEnumerable<T> source, Func<T, SqlDecimalBase> selector)
+		{
+			return Sum(source.Select(selector));
+		}
+	}
+}

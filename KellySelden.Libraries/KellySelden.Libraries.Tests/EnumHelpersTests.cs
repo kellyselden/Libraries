dddd8e106e196da9a24194180f1b2c4f620af07c
Kelly Selden <kellyselden@gmail.com>
2013-11-20 22:20:09 -0500

    enumerate an enum

---
+++ KellySelden.Libraries/KellySelden.Libraries.Tests/EnumHelpersTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using KellySelden.Libraries.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KellySelden.Libraries.Tests
+{
+	[TestClass]
+	public class EnumHelpersTests
+	{
+		enum Test { Test1 }
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var actual = EnumHelpers.ToEnumerable<Test>();
+
+			Assert.AreEqual(1, actual.Count());
+			Assert.AreEqual(Test.Test1, actual.ElementAt(0));
+		}
+	}
+}
---
+++ KellySelden.Libraries/KellySelden.Libraries/Helpers/EnumHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KellySelden.Libraries.Helpers
+{
+	public static class EnumHelpers
+	{
+		public static IEnumerable<T> ToEnumerable<T>()
+		{
+			return from object e in Enum.GetValues(typeof(T)) select (T)e;
+		}
+	}
+}

a8084796650f6d9f02780875f8af00c8404d9b3c
Kelly Selden <kellyselden@gmail.com>
2013-12-17 23:07:48 -0500

    IExcelService

---
+++ KellySelden.Libraries/KellySelden.Libraries.Excel/Domain/Entities/Row.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace KellySelden.Libraries.Excel.Domain.Entities
+{
+	public class Row
+	{
+		public IEnumerable<Cell> Cells { get; private set; }
+
+		public Row(IEnumerable<Cell> cells)
+		{
+			Cells = cells;
+		}
+	}
+}
---
+++ KellySelden.Libraries/KellySelden.Libraries.Excel/Domain/Entities/Workbook.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace KellySelden.Libraries.Excel.Domain.Entities
+{
+	public class Workbook
+	{
+		public IEnumerable<Worksheet> Worksheets { get; set; }
+
+		public Workbook(IEnumerable<Worksheet> worksheets)
+		{
+			Worksheets = worksheets;
+		}
+	}
+}
